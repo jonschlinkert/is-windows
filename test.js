af120edf9f7047434f48708755d3d79933c19936
jonschlinkert <jon.schlinkert@sellside.com>
2018-02-14 02:34:22 -0500

    update UMD wrapper and tests
    closes https://github.com/jonschlinkert/is-windows/issues/9

--- test.js
+++ test.js
@@ -4,12 +4,12 @@ require('mocha');
 var assert = require('assert');
 var isWindows = require('./');
 
-describe('isWindows', function () {
-  it('should return true if the platform is windows:', function () {
-    if (process && process.platform === 'win32') {
-      assert.equal(isWindows(), true);
-    } else {
+describe('isWindows', function() {
+  it('should return true if the platform is windows:', function() {
+    if (process && process.platform !== 'win32' && !/^(msys|cygwin)$/.test(process.env.OSTYPE)) {
       assert.equal(isWindows(), false);
+    } else {
+      assert.equal(isWindows(), true);
     }
   });
 });
